946785fcd12f972d0acf15974140a4b1bfe79984
ThomasD <thomas.dupiereux-fettweis@student.umons.ac.be>
2018-11-02 13:55:06 +0100

    Ex1

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,17 @@
 package exercise1;
 
 class Person {
+	private String name;
+	private int age;
+
+	public Person(String NAME, int AGE){
+		this.name = NAME;
+		this.age = AGE;
+	}
+	public int getAge(){
+		return age;
+	}
+	public String getName() {
+		return name;
+	}
 }
