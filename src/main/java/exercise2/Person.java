478dfa57239c7b15e6feaab507f871af1045dcd4
ThomasD <thomas.dupiereux-fettweis@student.umons.ac.be>
2018-11-02 14:45:14 +0100

    Ex2

--- src/main/java/exercise2/Person.java
+++ src/main/java/exercise2/Person.java
@@ -1,4 +1,28 @@
 package exercise2;
 
 class Person {
+	private int age;
+	private static int populationSize=0;
+	private static float totalAge=0;
+
+	public Person(int a){
+		age = a;
+		populationSize++;
+		totalAge = totalAge+a;
+	}
+
+	static int getPopulationSize() {
+		return populationSize;
+	}
+
+	static float getAveragePopulationAge() {
+		return totalAge/populationSize;
+	}
+
+	static void getResetPopulation() {
+		populationSize = 0;
+		totalAge = 0;
+	}
 }
+
+
